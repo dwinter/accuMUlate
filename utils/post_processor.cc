3656f2b314ab2db39091ca5de6412cc3ce18ad08
David Winter <Dvid.winter@gmail.com>
2014-03-10 17:08:04 -0700

    basic post-procsoor (allele freq counter) in place

--- utils/post_processor.cc
+++ utils/post_processor.cc
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <stdint.h>
-#include <map>
-#include <vector>
 #include <string>
-//#include <basic_string>
+#include <algorithm>
 #include "api/BamReader.h"
 #include "utils/bamtools_pileup_engine.h"
 
@@ -12,6 +10,7 @@
 using namespace BamTools;
 using namespace std;
 
+//TODO this is copy-pasted from parse_bam.cc. If is used more 
 uint16_t base_index(char b){
     switch(b){//TODO best 'null/npos' result for a short int?
         case 'A':
@@ -39,8 +38,8 @@ uint16_t base_index(char b){
 
 class FreqVisitor: public PileupVisitor{
     public:
-        FreqVisitor(BamAlignment ali):
-            PileupVisitor()  { }
+        FreqVisitor(BamAlignment ali, string chr, int ref_pos):
+            PileupVisitor(), m_ref_pos(ref_pos), m_chr(chr) { }
         ~FreqVisitor(void) { }
     public:
         void Visit(const PileupPosition& pileupData){
@@ -49,20 +48,37 @@ class FreqVisitor: public PileupVisitor{
             for(auto it = begin(pileupData.PileupAlignments);
                      it !=end(pileupData.PileupAlignments);
                      ++it){
-                int const *pos = &it->PositionInAlignment;
-                if(it->Alignment.Qualities[*pos] > 46){
-                    uint16_t b_index = base_index(it->Alignment.AlignedBases[*pos]);
-                    if (b_index < 4){
-                        bcalls.reads[b_index] += 1;
+                if(pileupData.Position == m_ref_pos){
+                    int const *pos = &it->PositionInAlignment;
+                    if(it->Alignment.Qualities[*pos] > 46){
+                        uint16_t b_index = base_index(it->Alignment.AlignedBases[*pos]);
+                        if (b_index < 4){
+                            bcalls.reads[b_index] += 1;
+                        }
                     }
                 }
             }
+            
+            int m = 0;
+            int nreads = 0;
             for(size_t i = 0; i < 4; i++){
-                cout << bcalls.reads[i] << '\t';
+                nreads += bcalls.reads[i];
+                if(bcalls.reads[i] > m){
+                    m = bcalls.reads[i];
+                }
             }
-            cout << endl;
-        }
+            if(nreads > 10){ 
+                double result = 1 - (m/(double)nreads);        
+                if(result > 0.05){ 
+                    cout << m_chr << '\t' << m_ref_pos << '\t' << result << '\t' << nreads << endl;                        
+                }
+            }
+                            
+            
+        }      
     private:
+        int m_ref_pos;
+        string m_chr;
 };
         
 
@@ -73,8 +89,6 @@ int main(int argc, char* argv[]){
     bam.Open(argv[2]);
     BamAlignment ali;
     PileupEngine pileup;
-    FreqVisitor *f = new FreqVisitor(ali);
-    pileup.AddVisitor(f);
     string L;
     while(getline(bed, L)){
         string chr_s;
@@ -91,10 +105,12 @@ int main(int argc, char* argv[]){
             pos_s.push_back(L[i]);
         }
         int pos = stoul(pos_s);
-        bam.SetRegion(chr,pos, chr, pos+1);
+        bam.SetRegion(chr,pos, chr, pos);
+        FreqVisitor *f = new FreqVisitor(ali, chr_s, pos);
+        pileup.AddVisitor(f); 
         while(bam.GetNextAlignment(ali)){
             pileup.AddAlignment(ali);
-        };
+        }
     };
     pileup.Flush();
     return 0;
