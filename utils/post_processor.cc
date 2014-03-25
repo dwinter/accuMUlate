cbe18d36150d9b6c32774e2c8c8d8aa3839734da
David Winter <david.winter@gmail.com>
2014-03-25 14:54:06 -0700

    Basic logic of post processor in place
    - Compiles (!)
    - Collects summary data for all samplesi (class SiteData)
    - Visitor then identifies mutant sample and mutant allele
    - Now need to summarise data for each and spit it out

--- utils/post_processor.cc
+++ utils/post_processor.cc
@@ -7,16 +7,16 @@
 #include "utils/bamtools_pileup_engine.h"
 
 #include "model.h"
+#include "parsers.h"
 
 using namespace std;
+using namespace BamTools;
 
-typedef vector<SiteData> SiteDataVector;
 
 class SiteData{
 
     public:
-        string name
-        uint16_t genotype
+        string name;
         vector<uint16_t> BQs;
         vector<uint16_t> MQs;
         uint32_t fwd_reads;
@@ -24,21 +24,21 @@ class SiteData{
         ReadData base_calls;
 
         SiteData(string sname){
-          name = string;
+          name = sname;
           base_calls.key = 0;
         }
                   
-        void call_genotype(){
+        int get_genotype(){
            //These have already been called for mutation-ness, and are haploid
            //so, to make a start, we are just calling the most common base
-           genotype = distance(base_calls.reads, max_element(base_calls.reads, 4));
+           return distance(base_calls.reads, max_element(base_calls.reads, base_calls.reads + 4 ));
         }
-        void add_alignment_data(const BamAlignment& al, const int& pos, const int& bindex){
-            uint16_t b_index = base_index(al.AlignedBases[*pos]);
+        void import_alignment(const BamAlignment& al, const int& pos, const int& bindex){
+            uint16_t b_index = base_index(al.AlignedBases[pos]);
             base_calls.reads[bindex] += 1;
-            BQs.push_back(al.Qualities[*pos]);
+            BQs.push_back(al.Qualities[pos]);
             MQs.push_back(al.MapQuality);
-            if(al.IsReverseStrand){ 
+            if(al.IsReverseStrand()){ 
                 rev_reads += 1; 
             } 
             else{ 
@@ -48,146 +48,88 @@ class SiteData{
 };
 
 
-
-string get_sample(string& tag){                                                           
-    string res;                                                                           
-    for(size_t i = 0; tag[i] != '_'; i++) {                                               
-        res.push_back(tag[i]);                                                            
-    }                                                                                     
-    return(res);                                                                          
+int sum(vector<int> input){
+    return(  accumulate(input.begin(), input.end(), 0) );
 }
 
-int find_sample_index(string s, SampleNames sv){                                          
-    for (size_t i=0; i < sv.size(); i++){                                                 
-        if(s.compare(sv[i])==0){                                                          
-            return i;                                                                     
-        }                                                                                 
-    }                                                                                     
-    cerr << "didn't find name " << s << endl;                                             
-    return(13); //TODO refactor this to  update sample in place                           
+double mean(vector<int> input){
+    double s = sum(input);
+    return(s/input.size());
 }
+        
+
+typedef vector<SiteData> SiteDataVector;
 
 class FilterVisitor: public PileupVisitor{
     public: 
-        FilterVisitor: public PileupVisitor(BamAlignment ali, 
-                                            ostream *out_stream,
-                                            SampleNames samples,
-                                            int ref_pos)
+        FilterVisitor(BamAlignment ali, ostream *out_stream,
+                      SampleNames samples, int ref_pos):
+
             PileupVisitor(), m_samples(samples), m_ostream(out_stream), 
                              m_ref_pos(ref_pos){
-                nsamp = samples.size(); 
+                nsamp = m_samples.size(); 
             } 
         ~FilterVisitor(void) { }
+    private:
+        SampleNames m_samples;
+        int m_ref_pos;
+        ostream* m_ostream;
+        int nsamp;
+
     public:
         void Visit(const PileupPosition pileupData){
             SiteDataVector sample_data;
-            for (size_t i = 0; i < nsamp i++){
-                site.push_back(SiteData(SampleNames[i]))
+            for (size_t i = 0; i < nsamp; i++){
+                sample_data.push_back(SiteData(m_samples[i]));
             }
             for (auto it =  pileupData.PileupAlignments.begin();
                       it != pileupData.PileupAlignments.end();
                       it++){
                 if(pileupData.Position == m_ref_pos){
                     int const *pos = &it->PositionInAlignment;
-                    if(it->Alignment.Qualities[*pos] > 46){//TODO deal with quality cut
-                        uint16_t b_index = base_index(it->Alignment.AlignedBases[*pos]);
-                        if (bindex < 4){
-                            int sindex = find_sample_index(get_sample(tag_id), m_samples);
-                            sample_data[sindex].add_alignment_data(it->Alignment, 
-                                                                    *pos, b_index)
-                                    
-                        }
-                    }
-                }
-            }
-            genotypes = vector<uint16_t>;
-            for(size_t i=0; i < nsamp; i++){
-                genotypes.push_back(sample_data[i].genotype());
-            }
-            // 
-                                        
-
-
-
-
-        
-
-class FreqVisitor: public PileupVisitor{
-    public:
-        FreqVisitor(BamAlignment ali, string chr, int ref_pos):
-            PileupVisitor(), m_ref_pos(ref_pos), m_chr(chr) { }
-        ~FreqVisitor(void) { }
-    public:
-        void Visit(const PileupPosition& pileupData){
-            ReadData bcalls;
-            bcalls.key = 0;
-            for(auto it = begin(pileupData.PileupAlignments);
-                     it !=end(pileupData.PileupAlignments);
-                     ++it){
-                if(pileupData.Position == m_ref_pos){
-                    int const *pos = &it->PositionInAlignment;
-                    if(it->Alignment.Qualities[*pos] > 46){
+                    if(it->Alignment.Qualities[*pos] > 46){//TODO user-defined qual cut to match first call?
                         uint16_t b_index = base_index(it->Alignment.AlignedBases[*pos]);
                         if (b_index < 4){
-                            bcalls.reads[b_index] += 1;
+                            string tag_id;
+                            it->Alignment.GetTag("RG", tag_id);
+                            uint32_t sindex = find_sample_index(get_sample(tag_id), m_samples);
+                            sample_data[sindex].import_alignment(it->Alignment, *pos, b_index);                        
                         }
                     }
                 }
             }
-            
-            int m = 0;
-            int nreads = 0;
-            for(size_t i = 0; i < 4; i++){
-                nreads += bcalls.reads[i];
-                if(bcalls.reads[i] > m){
-                    m = bcalls.reads[i];
-                }
+
+            vector<uint16_t> genotypes;
+            int gfreqs[4];
+            for(size_t i=0; i < nsamp; i++){
+                uint16_t g = sample_data[i].get_genotype();
+                gfreqs[g] += 1;
+                genotypes.push_back(g);                        
             }
-            if(nreads > 10){ 
-                double result = 1 - (m/(double)nreads);        
-                if(result > 0.05){ 
-                    cout << m_chr << '\t' << m_ref_pos << '\t' << result << '\t' << nreads << endl;                        
-                }
+            if(count(gfreqs, gfreqs+4, 1) == 1){//should only be one mutant
+                    // OK let's collect all that data....
+                    auto it = find_if(genotypes.begin(), genotypes.end(),
+                        [](int v) {return v==1;});
+                    unsigned mutant = *it;
+                    uint16_t mutant_base = genotypes[mutant];
+                    int mutant_alleles;
+                    int mutant_allele_denom;
+                    for (size_t i=0; i < nsamp; i++){
+                        if(i != mutant){
+                            mutant_alleles += sample_data[i].base_calls.reads[mutant_base];
+                            mutant_allele_denom += sample_data[i].fwd_reads + sample_data[i].rev_reads;
+                        }
+                    }
+                    double mutant_freq = (double)mutant_alleles/mutant_allele_denom;
+                    *m_ostream << mutant_freq << '\t' 
+                               << endl;
+
             }
-                            
-            
-        }      
-    private:
-        int m_ref_pos;
-        string m_chr;
+        }
+    
 };
-        
+                                 
 
 int main(int argc, char* argv[]){
-    fstream bed;
-    bed.open(argv[1]);
-    BamReader bam;
-    bam.Open(argv[2]);
-    BamAlignment ali;
-    PileupEngine pileup;
-    string L;
-    while(getline(bed, L)){
-        string chr_s;
-        size_t i = 0;
-        for(;L[i] != '\t';i++){
-            chr_s.push_back(L[i]);
-        }
-        int chr = bam.GetReferenceID(chr_s);
-
-
-        i+= 1;
-        string pos_s;
-        for(;L[i] != '\t'; i++){
-            pos_s.push_back(L[i]);
-        }
-        int pos = stoul(pos_s);
-        bam.SetRegion(chr,pos, chr, pos);
-        FreqVisitor *f = new FreqVisitor(ali, chr_s, pos);
-        pileup.AddVisitor(f); 
-        while(bam.GetNextAlignment(ali)){
-            pileup.AddAlignment(ali);
-        }
-    };
-    pileup.Flush();
     return 0;
 }
