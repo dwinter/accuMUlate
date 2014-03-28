b86e1d33b34145f614c38d6c1bcb7fad69e08440
David Winter <david.winter@gmail.com>
2014-03-28 13:35:40 -0700

    Redesign of post_processor
    Hopefully this represents a cleaner approach...
    
    -New classes represented site for one sample/whole experiment
    -Appropriate classes suck up alignment data/call genotypes/export summary
    -Visitor class now handles parsing BAM, only calls methods from other classes
    to process the results

--- utils/post_processor.cc
+++ utils/post_processor.cc
@@ -16,25 +16,29 @@ using namespace std;
 using namespace BamTools;
 
 
-class SiteData{
+class SampleSiteData{
 
     public:
-        string name;
         vector<uint16_t> BQs;
         vector<uint16_t> MQs;
         uint32_t fwd_reads;
         uint32_t rev_reads;
         ReadData base_calls;
 
-        SiteData(string sname){
-          name = sname;
+        SampleSiteData(){
           base_calls.key = 0;
         }
                   
         int get_genotype(){
            //These have already been called for mutation-ness, and are haploid
-           //so, to make a start, we are just calling the most common base
-           return distance(base_calls.reads, max_element(base_calls.reads, base_calls.reads + 4 ));
+           //so, to make a start, we are just calling the most common base          
+           uint16_t most_common = *max_element(base_calls.reads, base_calls.reads + 4 );
+           if (most_common==0){
+               return -1;
+           }
+           else{
+               return(distance(base_calls.reads, &most_common));
+           }
         }
         void import_alignment(const BamAlignment& al, const int& pos, const int& bindex){
             uint16_t b_index = base_index(al.AlignedBases[pos]);
@@ -50,114 +54,135 @@ class SiteData{
         }
 };
 
-
  
+class ExperimentSiteData{
+    public:
+        vector<SampleSiteData> sample_data;
+        SampleNames snames;
 
-typedef vector<SiteData> SiteDataVector;
+        ExperimentSiteData(SampleNames sn){
+            snames = sn;           
+            //fill constructor doesn't work here?
+            for(size_t i= 0; i < sn.size(); i++){
+                sample_data.push_back(SampleSiteData());
+            }
+        }
+
+        void summarize(ostream* out_stream){
+            // call genotypes, keep track of each sample and number of each 
+            // possible allele
+            vector<uint16_t> genotypes;
+            int gfreqs[4];
+            for(size_t i=0; i < sample_data.size(); i++){
+                uint16_t g = sample_data[i].get_genotype();
+                if(g < 4){//no data == npos
+                    gfreqs[g] += 1;
+                }
+                genotypes.push_back(g);                        
+            }
+            //Now find the mutant (should be the only one with it's allele)
+            uint16_t mutant_base;
+            uint16_t n_mutant;
+            for(size_t i = 0; i<4; i++){
+                if (gfreqs[i] == 1){
+                    n_mutant += 1;
+                    mutant_base = i;
+                }
+            }
+            if (n_mutant != 1){
+                return; // warn?
+            }
+            auto it = find_if(genotypes.begin(), genotypes.end(), 
+                    [&](int v) {return v==mutant_base;});
+            uint32_t mutant = *it;
+            //summarise the data
+
+            int mutant_alleles;
+            int mutant_alleles_denom;
+            int wt_MQs;
+            int wt_MQs_denom;
+            auto s_mutant_MQs = accumulate (sample_data[mutant].MQs.begin(),
+                                            sample_data[mutant].MQs.end(), 0);
+            double mutant_MQs = s_mutant_MQs/(double)sample_data[mutant].MQs.size();
+            for (size_t i=0; i < sample_data.size(); i++){
+                if (i != mutant){
+                    mutant_alleles += sample_data[i].base_calls.reads[mutant_base];
+                    mutant_alleles_denom += sample_data[i].fwd_reads + sample_data[i].rev_reads;
+                    wt_MQs += accumulate(sample_data[i].MQs.begin(), sample_data[i].MQs.end(), 0); 
+                    wt_MQs_denom += sample_data[i].MQs.size();     
+                }
+            double xbar_MQs = (double)wt_MQs/wt_MQs_denom;
+            double mutant_freq = (double)mutant_alleles/mutant_alleles_denom;
+            *out_stream  << mutant_base << '\t'
+                          << snames[mutant] << '\t'
+                          << mutant_freq << '\t' 
+                          << sample_data[mutant].fwd_reads << '\t'
+                          << sample_data[mutant].rev_reads << '\t'
+                          << mutant_MQs << '\t'
+                          << xbar_MQs  << endl;
+            }
+        }
+};
+
 
 class FilterVisitor: public PileupVisitor{
     public: 
         FilterVisitor(BamAlignment& ali, 
-                      ostream *out_stream,
-                      SampleNames samples, 
-                      int ref_pos,
-                      string initial_data):
-
-            PileupVisitor(), m_samples(samples), m_ostream(out_stream), 
-                             m_ref_pos(ref_pos), m_initial_data(initial_data){
-                nsamp = m_samples.size(); 
-            } 
+                      const SampleNames& samples, 
+                      ostream* out_stream,
+                      int ref_pos):
+            PileupVisitor(), m_samples(samples), m_ref_pos(ref_pos), m_out_stream(out_stream)
+            {  } 
         ~FilterVisitor(void) { }
 
 
     public:
         void Visit(const PileupPosition& pileupData){
+            auto target_site = ExperimentSiteData(m_samples);
             for (auto it =  pileupData.PileupAlignments.begin();
                       it != pileupData.PileupAlignments.end();
                       it++){
+                if(pileupData.Position > m_ref_pos){
+                //PileupEngine takes all alignments that _overlap_ our target
+                //once we've gone past the target we are done so:
+                    break;
+                }
                 if(pileupData.Position == m_ref_pos){
-                    for (size_t i = 0; i < nsamp; i++){
-                        sample_data.push_back(SiteData(m_samples[i]));
-                    }
                     int const *pos = &it->PositionInAlignment;
                     if(it->Alignment.Qualities[*pos] > 46){//TODO user-defined qual cut to match first call?
                         uint16_t b_index = base_index(it->Alignment.AlignedBases[*pos]);
                         if (b_index < 4){
                             string tag_id;
                             it->Alignment.GetTag("RG", tag_id);
-                            uint32_t sindex = find_sample_index(get_sample(tag_id), m_samples);
-                            sample_data[sindex].import_alignment(it->Alignment, *pos, b_index);                        
+                            uint32_t sindex = find_sample_index(get_sample(tag_id),m_samples);
+                            target_site.sample_data[sindex].import_alignment(it->Alignment, *pos, b_index);               
                         }
                     }
                 }
             }
-                
-
-            vector<uint16_t> genotypes;
-            int gfreqs[4];
-            for(size_t i=0; i < nsamp; i++){
-                uint16_t g = sample_data[i].get_genotype();
-                gfreqs[g] += 1;
-                genotypes.push_back(g);                        
-            }
-            if(count(gfreqs, gfreqs+4, 1) == 1){//should only be one mutant
-            // OK let's collect all that data....
-                auto it = find_if(genotypes.begin(), genotypes.end(),
-                            [](int v) {return v==1;});
-                unsigned mutant = *it;
-                uint16_t mutant_base = genotypes[mutant];
-                int mutant_alleles;
-                int mutant_allele_denom;
-                int wt_MQs;
-                int wt_MQ_denom;
-                int mutant_MQs =  accumulate(sample_data[mutant].MQs.begin(), sample_data[mutant].MQs.end(), 0) /
-                                    sample_data[mutant].MQs.size();
-                for (size_t i=0; i < nsamp; i++){
-                    if(i != mutant){
-                        mutant_alleles += sample_data[i].base_calls.reads[mutant_base];
-                        mutant_allele_denom += sample_data[i].fwd_reads + sample_data[i].rev_reads;
-                        wt_MQs += accumulate(sample_data[i].MQs.begin(), sample_data[i].MQs.end(), 0); 
-                        wt_MQ_denom += sample_data[i].MQs.size();     
-                    }
-                            
-                }
-                double xbar_MQs = double(wt_MQs/wt_MQ_denom);
-                double mutant_freq = (double)mutant_alleles/mutant_allele_denom;
-                *m_ostream  << m_initial_data << '\t'
-                            << mutant_base << '\t'
-                            << sample_data[mutant].name << '\t'
-                            << mutant_freq << '\t' 
-                            << sample_data[mutant].fwd_reads << '\t'
-                            << sample_data[mutant].rev_reads << '\t'
-                            << mutant_MQs << '\t'
-                            << xbar_MQs  << endl;
-            }
-                
+            target_site.summarize(m_out_stream);
         }
 
     private:
         SampleNames m_samples;
-        SiteDataVector sample_data;        
+        ostream* m_out_stream;
         int m_ref_pos;
-        ostream* m_ostream;
-        int nsamp;
-        string m_initial_data;
+       // ExperimentSiteData target_site;
     
 };
-                                 
+
 
 int main(int argc, char* argv[]){
     string bam_path;
     string input_path;
-    string ref_path;
+    vector< string > sample_names;
     namespace po = boost::program_options;
     po::options_description cmd("Command line args");
     cmd.add_options()
         ("help,h", "Print a help message")
         ("bam,b", po::value<string>(&bam_path)->required(), "Path to BAM file")
-        ("reference,r", po::value<string>(&ref_path)->required(), "Path to reference genome")
         ("input,i", po::value<string>(&input_path)->required(), "Path to results file")
-        ("sample-name,s", po::value<vector <string> >()->required(), "Sample tags")
+        ("sample-name,s", po::value<vector <string> >(&sample_names)->required(), "Sample tags")
         ("config,c", po::value<string>(), "Path to config file")
         ("out,o", po::value<string>()->default_value("filtered_result.tsv"),
                     "Out file name");
@@ -179,17 +204,16 @@ int main(int argc, char* argv[]){
     vm.notify();
 
     ofstream outfile (vm["out"].as<string>());
+
     ifstream putations(input_path);
     BamReader experiment;
     experiment.Open(bam_path);
-    //FastaReference ref_genome (ref_path + ".fai");
     string L;
-    PileupEngine pileup;
-    BamAlignment ali;
-
     while(getline(putations, L)){    
+    
+        PileupEngine pileup;
+        BamAlignment ali;
 
-        // chr \t pos \t ref \t prob
         size_t i = 0;
         string chr;
         for(; L[i] != '\t'; ++i){
@@ -202,17 +226,17 @@ int main(int argc, char* argv[]){
         }
         int pos = stoul(pos_s);
         int ref_id = experiment.GetReferenceID(chr);
-        experiment.SetRegion(ref_id, pos, ref_id, pos+1);
+        experiment.SetRegion(ref_id, pos-1, ref_id, pos);
         FilterVisitor *f = new FilterVisitor(ali, 
+                                             sample_names,
                                              &outfile,
-                                             vm["sample-name"].as<vector< string> >(), 
-                                             pos, L);
+                                             pos);
         pileup.AddVisitor(f);
         while( experiment.GetNextAlignment(ali) ) {
             pileup.AddAlignment(ali);
         }
+        pileup.Flush();
     }
-    pileup.Flush();
     return 0;
 }
 
