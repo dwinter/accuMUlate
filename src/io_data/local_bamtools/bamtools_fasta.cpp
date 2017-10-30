cd957a1ea27caf6e3faad101e02e824369569b17
David Winter <david.winter@gmail.com>
2017-10-30 13:30:51 +1300

    test travis

--- src/io_data/local_bamtools/bamtools_fasta.cpp
+++ src/io_data/local_bamtools/bamtools_fasta.cpp
@@ -17,7 +17,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include "bamtools/shared/bamtools_global.h"
+#include <bamtools/shared/bamtools_global.h>
 
 using namespace std;
 using namespace LocalBamToolsUtils;
